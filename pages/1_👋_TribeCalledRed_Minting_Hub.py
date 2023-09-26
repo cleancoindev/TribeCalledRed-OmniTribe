285bd5c9d5f832b919e1c1fc77d8d0daaf83febc
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-09-25 19:22:29 -1000

    Update and rename 1_👋_Maui_Fundrazer_Hub.py to 1_👋_TribeCalledRed_Minting_Hub.py

---
+++ pages/1_👋_TribeCalledRed_Minting_Hub.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Halluci-Nation Info Hub",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Art Token TurtleShell Information Hub. 👋")
+
+st.markdown(
+    """
+    ### Want to learn more?
+    - Check out [ Token for Art purchase and donation to the tribes! Demo](https:///)
+
+
+"""
+)
