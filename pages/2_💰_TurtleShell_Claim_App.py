cb8bc316874670262c5a0bcd99834e05937baea0
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-09-25 19:23:31 -1000

    Update and rename 2_💰_7MTFUND_Claim_App.py to 2_💰_TurtleShell_Claim_App.py

---
+++ pages/2_💰_TurtleShell_Claim_App.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TurtleShell Matic Claim Page",
+    page_icon="💰",
+)
+
+st.write("# Welcome to TribeCalledRed Unofficial Fundraising App Information Page! 💰")
+
+st.markdown(
+    """
+    ### Want to learn more?
+    - Check out https:// link to token drop purchase page, price starting 200 matic/
+
+"""
+)
