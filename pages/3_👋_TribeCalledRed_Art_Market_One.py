627c7fea0cf3985300499d210be2367b72e448aa
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-09-25 19:24:57 -1000

    Update and rename 3_👋_Maui_Art_Market_One.py to 3_👋_TribeCalledRed_Art_Market_One.py

---
+++ pages/3_👋_TribeCalledRed_Art_Market_One.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TribeCalledRed NFT KEY Minter for Tribal Affiliate Registration",
+    page_icon="👋",
+)
+
+st.write("# Welcome to the Omni-Tribe Registration Key Minter! 👋")
+
+st.markdown(
+    """
+    ### 
+    - Check out [the key minter below!]
+
+
+"""
+)
