24dbab71c576b946dbe62b37277c35dda3657804
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-09-25 23:28:07 -1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,8 +25,10 @@ c12.image(Image.open('images/optimism-logo.png'))
 c13.image(Image.open('images/arbitrum-logo.png'))
 c14.image(Image.open('images/axelar-logo.png'))
 
-c16, = st.columns(1)
-c16.image(Image.open('images/axelar-logo.png'))
+c16, c17= st.columns(2)
+c16.image(Image.open('images/.png'))
+c17.image(Image.open('images/.png'))
+
 
 st.write(
     """
