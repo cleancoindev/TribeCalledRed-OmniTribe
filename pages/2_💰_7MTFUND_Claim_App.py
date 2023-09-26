98b0aa6ae2766b8b852dc98dfa61d1c51d7e6b51
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-09-25 19:21:05 -1000

    Initial commit

---
+++ pages/2_💰_7MTFUND_Claim_App.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Maui-Fundraze Matic Claim",
+    page_icon="💰",
+)
+
+st.write("# Welcome to Maui-Fundrazer Fund App Information Page! 💰")
+
+st.markdown(
+    """
+    ### Want to learn more?
+    - Check out https:// link to token drop purchase page, price starting 10 matic/
+
+"""
+)
